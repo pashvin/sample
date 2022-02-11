acb76ca84d187a248c059e4a7a362e8995baf311
Ashvin Patel <ashvin.patel@mitel.com>
2022-02-10 21:15:38 -0800

    Update reverse-list.ts

--- algo/reverse-list.ts
+++ algo/reverse-list.ts
@@ -1,7 +1,21 @@
-import {LNode} from './list-node';
+import { LNode } from "./list-node";
 
-let head = LNode.parseData([1,2,3,4,5]);
+let head = LNode.parseData([1, 2, 3, 4, 5]);
 
+console.log("original:")
 LNode.print(head);
 
+let reverseNode = (head: LNode): LNode => {
+    let current = head;
+    let prev = null;
+    while(current !== null) {
+        let temp = current.next;
+        current.next = prev;
+        prev = current;
+        current = temp;
+    }
+    return prev;
+};
 
+console.log("reverse list:")
+LNode.print(reverseNode(head));
