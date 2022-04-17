4c8f6ca18a6c0ea06a28eb09ffe0f392cef463ab
Ashvin Patel <ashvin.patel@mitel.com>
2022-04-16 18:04:38 -0700

    Added Singleton example

--- design-patterns/src/app/routes/pattern2/pattern2.component.ts
+++ design-patterns/src/app/routes/pattern2/pattern2.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LoggerService } from 'src/app/service/logger.service';
 
 @Component({
   selector: 'app-pattern2',
@@ -7,9 +8,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Pattern2Component implements OnInit {
 
-  constructor() { }
+  constructor(private logger: LoggerService) { }
 
   ngOnInit(): void {
   }
 
+  run() {
+    const s1 = Singleton.getInstance();
+    const s2 = Singleton.getInstance();
+
+    if (s1 === s2) {
+        this.logger.addLog('Singleton works, both variables contain the same instance.');
+    } else {
+      this.logger.addLog('Singleton failed, variables contain different instances.');
+    }
+  }
+
+}
+
+class Singleton {
+  private static instance: Singleton;
+
+  // make constructor private
+  private constructor() { }
+
+  // expose method which can call constructor but once
+  public static getInstance(): Singleton {
+      if (!Singleton.instance) {
+          Singleton.instance = new Singleton();
+      }
+      return Singleton.instance;
+  }
+
+  /**
+   * Finally, any singleton should define some business logic, which can be
+   * executed on its instance.
+   */
+  public someBusinessLogic() {
+      // ...
+  }
 }
