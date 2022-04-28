946eb98f48a46a9e45a1cfe16105922a10818f3d
Ashvin Patel <ashvin.patel@mitel.com>
2022-04-27 18:31:03 -0700

    Added React form project

---
+++ model-binding/react/src/App.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import './App.css';
+import { useForm } from "react-hook-form";
+
+// function App() {
+//   return (
+//     <div>
+//       Hello
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+type FormData = {
+  firstName: string;
+  lastName: string;
+};
+
+export default function App() {
+  const { register, setValue, handleSubmit, formState: { errors } } = useForm<FormData>();
+  const onSubmit = handleSubmit(data => {
+    alert("Hello: " + data.firstName + " " + data.lastName );
+    console.log(data);
+  });
+  // firstName and lastName will have correct type
+
+  return (
+    <form onSubmit={onSubmit}>
+      <label>First Name</label>
+      <input {...register("firstName")} />
+      <label>Last Name</label>
+      <input {...register("lastName")} />
+      <button type="submit">Save data</button>
+      <button
+        type="button"
+        onClick={() => {
+          setValue("lastName", "Luo"); 
+          setValue("firstName", "Jake"); 
+        }}
+      >
+        SetValue
+      </button>
+    </form>
+  );
+}
