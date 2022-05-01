0ecdb32fef550d95a09280dde588e5609886c579
Ashvin Patel <ashvin.patel@mitel.com>
2022-05-01 10:46:14 -0700

    Update google.tsx

--- pwa-graph/src/charts/google/google.tsx
+++ pwa-graph/src/charts/google/google.tsx
@@ -1,5 +1,64 @@
+import React, { useContext, useEffect, useState } from "react";
+
+declare const google: any; 
+
 export default function GoogleChat() {
-    return(
-        <div>Google Sample</div>
-    );
+  const loadScript = () => {
+    return new Promise((res: any, rej: any) => {
+      const existingScript = document.getElementById("googleCharts");
+      if (!existingScript) {
+        const script = document.createElement("script");
+        script.src = "https://www.gstatic.com/charts/loader.js";
+        script.id = "googleCharts";
+        document.body.appendChild(script);
+        script.onload = () => {
+          res("loaded");
+        };
+      }
+      res("already loaded");
+    });
+  };
+
+  const createGraph = () => {
+    google.charts.load('current', {'packages':['corechart']});
+    google.charts.setOnLoadCallback(drawChart);
+
+    function drawChart() {
+      var data = google.visualization.arrayToDataTable([
+        ['Year', 'Sales', 'Expenses'],
+        ['2004',  1000,      400],
+        ['2005',  1170,      460],
+        ['2006',  660,       1120],
+        ['2007',  1030,      540]
+      ]);
+
+      var options = {
+        title: 'Company Performance',
+        curveType: 'function',
+        legend: { position: 'bottom' }
+      };
+
+      var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
+
+      chart.draw(data, options);
+    }
+  }
+
+  useEffect(() => {
+    // React 18 loads, unload , reload component in debug mode o detect 
+    // leaks. This is workaround to call useEffect only once
+    let timer = setInterval(_=>{
+        clearInterval(timer);
+        loadScript().then(result=> {
+            createGraph();
+        });
+    },1000);
+    return function cleanup() {
+      if(timer) {
+          clearInterval(timer);
+      }
+    };
+  }, []); // pass second arg as [] to avoid redraw on set status
+
+  return  <div id="curve_chart" style={{width:900, height:500}}></div>;
 }
